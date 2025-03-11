ac88e4e02a0e6c3f9cdbf64fa45b6401b66c5181
duomiduole <30642150+duomiduole@users.noreply.github.com>
2025-03-11 03:36:34 +0000

    feat: update

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'build',
-  basePath: 'build',
+  basePath: '/build',
 };
 
 export default nextConfig;
